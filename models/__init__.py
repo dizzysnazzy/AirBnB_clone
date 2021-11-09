1b26c57630809e6994da17ad5798a01cb107f9f0
Derrick Ampire <ampire90@gmail.com>
2021-11-09 17:20:53 +0000

    updated to create FileStorage

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Initializes the model package and and creates a unique FileStorage instance
+"""
+
+from model.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
