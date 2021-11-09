d78ea272280b9d1bfd7572d555ffbd67904bb92d
Derrick Ampire <ampire90@gmail.com>
2021-11-09 17:49:32 +0000

    Console 0.0.1 tasks

--- console.py
+++ console.py
@@ -2,3 +2,22 @@
 """ the console """
 
 import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """the hbnb console"""
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """implements an empty line and ENTER"""
+        return False
+
+    def do_quit(self, arg):
+        """"implementation of quit command"""
+        return True
+    
+    def do_EOF(self, arg):
+        """implemenatation of EOF to exit"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
