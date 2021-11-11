28aeddf3daf384ec5852f3677719e9c63f354859
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-11-11 05:47:54 -0800

    airbnb

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 Initializes the model package and and creates a unique FileStorage instance
 """
-
 from model.engine.file_storage import FileStorage
-
-=======
-"""Module for FileStorage autoinit."""
-from models.engine.file_storage import FileStorage
->>>>>>> main
 storage = FileStorage()
 storage.reload()
