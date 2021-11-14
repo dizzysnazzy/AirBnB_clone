e5ff9552599a69884a0ac67538392be4efac55cf
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-11-14 15:26:14 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,10 @@
 
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
     """Class representing a Amenity."""
     name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
