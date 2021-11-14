56457516f6ff65a74229aec7167d3af105e05098
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-11-14 15:33:13 +0300

    Initialized file

--- models/state.py
+++ models/state.py
@@ -3,7 +3,10 @@
 
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
     """Class representing a State."""
     name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
